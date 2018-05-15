5e69124781789904a127c60825bda6a4169c6ce4
Nick Drishinski <ndrishinski@yahoo.com>
2018-05-14 19:30:31 -0600

    Installed Petergate gem

--- i-am-balancer/config/routes.rb
+++ i-am-balancer/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
-  
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root to: 'pages#home'
 
   get 'pages/about'
